bd8b83313d467a8c4eb806a38888e84e60d05017
RupaliNargole <100370025+RupaliNargole@users.noreply.github.com>
2022-02-25 00:57:58 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+#add
+def add(x,y):
+  return x + y
+#sub
+def sub(x,y):
+  return(x-y)
+#mul
+def multiply(x,y):
+  return(x*y)     #on bug456
+#divide
+def divide(x,y):
+
+  if y ==0
+   else x/y    master bug 789
+#square
+def square()
+return (x*x)
